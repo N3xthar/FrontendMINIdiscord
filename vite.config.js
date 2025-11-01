ba45a0c54872ff8ab2ba94bfdbb8d45b0850525d
N3xthar <n3xtahrvory4x@gmail.com>
2025-11-02 00:59:19 +0530

    Trying to oslve the error

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist',
   },
-  server: {
-    port: 3000,
-  },
+  publicDir: resolve(__dirname, 'public'),
 })
